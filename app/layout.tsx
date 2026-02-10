fa64b1c1086c5fabc6da09cd8e7211ed08ee225e
kurogami20 <dam31vt@gmail.com>
2026-02-10 15:56:23 +0100

    sidebar added to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "../components/theme-provider";
+import Header from "@/components/header";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,12 +27,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,16 @@
+import { AppSidebar } from "./appSidebar";
+import { SidebarProvider, SidebarTrigger } from "./ui/sidebar";
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <header>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarTrigger />
+      </SidebarProvider>
+      <h1>Damien Chabanier Portfolio</h1>
+    </header>
+  );
 };
 
 export default Header;
